627bda650c9d9d43da9e18bd8e534f727084ed43
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-09-01 04:17:08 +0900

    init

---
+++ src/scripts/app.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers'
+import { Provider } from 'react-redux'
+import createLogger from 'redux-logger'
+import thunk        from 'redux-thunk'
+const logger = createLogger();
+
+const store = createStore(reducers, applyMiddleware( thunk, logger ) );
+const container = document.querySelector('#app');
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	container
+);
---
+++ src/scripts/reducers/index.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux'
+
+const reducer = (state = {}, action) => {
+
+	switch (action.type) {
+
+		default:
+			return state
+	}
+}
+
+
+const reducers = combineReducers({
+	reducer
+});
+
+export default reducers;
